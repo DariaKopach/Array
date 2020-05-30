709ea174c4cf0cc1c0a5c8517e97efece6704212
DariaArk <dariyakopch@gmail.com>
2020-05-30 22:39:34 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+/*
+ * Cycle practice
+ *
+ * Version 1.0
+ *
+ * Copyright Kopach Daria KNTEU
+ */
+
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String text = new String(Files.readAllBytes(Paths.get("C:\\Users\\Nastya\\Desktop\\Harry.txt")));
+
+        String cleanerText = text
+                .replaceAll("\\.", "")
+                .replaceAll(",", "")
+                .replaceAll("\"", "")
+                .replaceAll("!", "")
+                .replaceAll(";", "")
+                .replaceAll(":", "")
+                .replaceAll("\\?", "")
+                .replaceAll("\\(", "")
+                .replaceAll("\\)", "")
+                .replaceAll("\\!", "")
+                .replaceAll("\"", "")
+                .replaceAll("-", "")
+                .replaceAll("'", "")
+                .replaceAll("\n", "")
+                .replaceAll("\\d", "")
+                .toLowerCase();
+
+        String[] words = cleanerText.split(" ");
+
+        String distinctString = " ";
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (!distinctString.contains(words[i])) {
+                distinctString += words[i] + " ";
+            }
+
+        }
+
+        // System.out.println(distinktString);
+
+        String[] distinctArray = distinctString.split(" ");
+        Arrays.sort(distinctArray);
+
+        for (int i = 0; i < distinctArray.length; i++) {
+            System.out.println(distinctArray[i]);
+        }
+        String hw = "Hello World!";
+        int hash = hw.hashCode();
+        System.out.println(hash);
+    }
+}
